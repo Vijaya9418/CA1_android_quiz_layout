d0b2fc589f0decdf50bf3d11b6d833701460fcb6
vijaya shree kanwal <56352158+Vijaya9418@users.noreply.github.com>
2021-02-27 19:11:29 +0530

    Update reg.java

--- app/src/main/java/com/example/ca1_android/reg.java
+++ app/src/main/java/com/example/ca1_android/reg.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
